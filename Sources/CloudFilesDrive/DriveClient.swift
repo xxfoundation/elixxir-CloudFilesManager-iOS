08b4b7a1ce955a7ec9ae12ad56eaadea84651211
Bruno Muniz Azevedo Filho <bruno@elixxir.io>
2022-10-09 19:58:19 -0300

    add upload and fetch for drive

--- Sources/CloudFilesDrive/DriveClient.swift
+++ Sources/CloudFilesDrive/DriveClient.swift
@@ -4,27 +4,26 @@ import GoogleSignIn
 import GoogleAPIClientForREST_Drive
 
 public struct DriveClient {
-  typealias FetchCompletion = (Result<Fetch.Metadata, Swift.Error>) -> Void
   typealias SignInCompletion = (Result<Void, Swift.Error>) -> Void
   typealias AuthorizeCompletion = (Result<Void, Swift.Error>) -> Void
-  typealias ListFilesCompletion = (Result<Void, Swift.Error>) -> Void
-  typealias ListFolderCompletion = (Result<String, Swift.Error>) -> Void
+  typealias FetchCompletion = (Result<Fetch.Metadata?, Swift.Error>) -> Void
+  typealias UploadCompletion = (Result<Upload.Metadata, Swift.Error>) -> Void
 
   public enum DriveClientError: Swift.Error {
     case unknown
     case missingScopes
     case fetch(Error)
     case signIn(Error)
+    case upload(Error)
     case authorize(Error)
-    case listFiles(Error)
-    case listFolder(Error)
   }
+
+  static var service = GTLRDriveService()
 
   var _unlink: () -> Void
   var _isLinked: () -> Bool
   var _fetch: (String, @escaping FetchCompletion) -> Void
-  var _listFiles: (String, @escaping ListFilesCompletion) -> Void
-  var _listFolder: (String, @escaping ListFolderCompletion) -> Void
+  var _upload: (String, Data, @escaping UploadCompletion) -> Void
   var _authorize: (UIViewController, @escaping AuthorizeCompletion) -> Void
   var _signIn: (String, String, UIViewController, @escaping SignInCompletion) -> Void
 
@@ -36,18 +35,12 @@ public struct DriveClient {
     _isLinked()
   }
 
-  func listFolder(
-    name: String,
-    completion: @escaping ListFolderCompletion
+  func upload(
+    path: String,
+    input: Data,
+    completion: @escaping UploadCompletion
   ) {
-    _listFolder(name, completion)
-  }
-
-  func listFiles(
-    at folder: String,
-    completion: @escaping ListFilesCompletion
-  ) {
-    _listFiles(folder, completion)
+    _upload(path, input, completion)
   }
 
   func fetch(
@@ -75,136 +68,128 @@ public struct DriveClient {
 }
 
 extension DriveClient {
-  public static func live() -> DriveClient {
-    let service = GTLRDriveService()
-    return DriveClient(
-      _unlink: {
-        GIDSignIn.sharedInstance.signOut()
-      },
-      _isLinked: {
-        let fileScope = "https://www.googleapis.com/auth/drive.file"
-        let appDataScope = "https://www.googleapis.com/auth/drive.appdata"
-        guard let currentUser = GIDSignIn.sharedInstance.currentUser,
-              let grantedScopes = currentUser.grantedScopes,
-              grantedScopes.contains(fileScope),
-              grantedScopes.contains(appDataScope) else { return false }
-        return true
-      },
-      _fetch: { fileName, completion in
-        let query = GTLRDriveQuery_FilesList.query()
-        query.q = "'\("folder")' in parents and name = '\(fileName)'"
-        query.spaces = "appDataFolder"
-        query.fields = "nextPageToken, files(id, size, name, modifiedTime)"
-        service.executeQuery(query) { _, result, error in
-          if let error {
-            completion(.failure(DriveClientError.fetch(error)))
+  public static let live = DriveClient(
+    _unlink: {
+      GIDSignIn.sharedInstance.signOut()
+    },
+    _isLinked: {
+      guard let currentUser = GIDSignIn.sharedInstance.currentUser,
+            let grantedScopes = currentUser.grantedScopes,
+            grantedScopes.contains(kGTLRAuthScopeDriveFile),
+            grantedScopes.contains(kGTLRAuthScopeDriveAppdata) else { return false }
+      return true
+    },
+    _fetch: { fileName, completion in
+      let query = GTLRDriveQuery_FilesList.query()
+      query.q = "name = '\(fileName)'"
+      query.spaces = "appDataFolder"
+      query.fields = "files(size, modifiedTime)"
+      service.executeQuery(query) { _, result, error in
+        if let error {
+          if (error as NSError).domain == kGTLRErrorObjectDomain, (error as NSError).code == 404 {
+            completion(.success(nil))
             return
           }
-          guard let metadata = (result as? GTLRDrive_FileList)?.files?.first, let size = metadata.size?.floatValue else {
-            completion(.failure(DriveClientError.unknown))
-            return
-          }
-          completion(.success(.init(
-            size: size,
-            lastModified: metadata.modifiedTime?.date
-          )))
-        }
-      },
-      _listFiles: { folderId, completion in
-        let query = GTLRDriveQuery_FilesList.query()
-        query.q = "'\(folderId)' in parents"
-        query.spaces = "appDataFolder"
-        query.fields = "nextPageToken, files(id, modifiedTime, size, name)"
-        service.executeQuery(query) { _, result, error in
-          if let error {
-            completion(.failure(DriveClientError.listFiles(error)))
-            return
-          }
-          guard let files = (result as? GTLRDrive_FileList)?.files else {
-            completion(.failure(DriveClientError.unknown))
-            return
-          }
-          let metadataList = files.map { _ in }
-        }
-      },
-      _listFolder: { folderName, completion in
-        let query = GTLRDriveQuery_FilesList.query()
-        query.q = "mimeType = 'application/vnd.google-apps.folder' and name = '\(folderName)'"
-        query.spaces = "appDataFolder"
-        query.fields = "nextPageToken, files(id, name)"
-        service.executeQuery(query) { _, result, error in
-          if let error {
-            completion(.failure(DriveClientError.listFolder(error)))
-            return
-          }
-          guard let folderId = (result as? GTLRDrive_FileList)?.files?.first?.identifier else {
-            completion(.failure(DriveClientError.unknown))
-            return
-          }
-          completion(.success(folderId))
-        }
-      },
-      _authorize: { controller, completion in
-        guard let user = GIDSignIn.sharedInstance.currentUser,
-              let scopes = user.grantedScopes else {
-          completion(.failure(DriveClientError.missingScopes))
+          completion(.failure(DriveClientError.fetch(error)))
           return
         }
-        service.authorizer = user.authentication.fetcherAuthorizer()
-        let fileScope = "https://www.googleapis.com/auth/drive.file"
-        let appDataScope = "https://www.googleapis.com/auth/drive.appdata"
-        if !scopes.contains(fileScope) || !scopes.contains(appDataScope) {
-          GIDSignIn.sharedInstance.addScopes([
-            fileScope, appDataScope
-          ], presenting: controller, callback: { user, error in
-            if let error {
-              completion(.failure(DriveClientError.authorize(error)))
-              return
-            }
-            completion(.success(()))
-          })
-        } else {
+        guard let metadata = (result as? GTLRDrive_FileList)?.files?.first,
+              let size = metadata.size?.floatValue else {
+          completion(.failure(DriveClientError.unknown))
+          return
+        }
+        completion(.success(.init(
+          size: size,
+          lastModified: metadata.modifiedTime?.date
+        )))
+      }
+    },
+    _upload: { path, data, completion in
+      let file = GTLRDrive_File(json: [
+        "name":"backup.xxm",
+        "parents": ["appDataFolder"],
+        "mimeType": "application/octet-stream"
+      ])
+      let params = GTLRUploadParameters(data: data, mimeType: "application/octet-stream")
+      let query = GTLRDriveQuery_FilesCreate.query(withObject: file, uploadParameters: params)
+      query.fields = "size, modifiedTime"
+      service.executeQuery(query) { _, result, error in
+        if let error {
+          completion(.failure(DriveClientError.upload(error)))
+          return
+        }
+        guard let file = result as? GTLRDrive_File,
+              let size = file.size?.floatValue,
+              let date = file.modifiedTime?.date else {
+          completion(.failure(DriveClientError.unknown))
+          return
+        }
+        completion(.success(.init(
+          size: size,
+          lastModified: date
+        )))
+      }
+    },
+    _authorize: { controller, completion in
+      guard let user = GIDSignIn.sharedInstance.currentUser,
+            let scopes = user.grantedScopes else {
+        completion(.failure(DriveClientError.missingScopes))
+        return
+      }
+      service.authorizer = user.authentication.fetcherAuthorizer()
+      if !scopes.contains(kGTLRAuthScopeDriveFile) ||
+          !scopes.contains(kGTLRAuthScopeDriveAppdata) {
+        GIDSignIn.sharedInstance.addScopes([
+          kGTLRAuthScopeDriveAppdata,
+          kGTLRAuthScopeDriveFile,
+        ], presenting: controller, callback: { user, error in
+          if let error {
+            completion(.failure(DriveClientError.authorize(error)))
+            return
+          }
+          completion(.success(()))
+        })
+      } else {
+        completion(.success(()))
+      }
+    },
+    _signIn: { apiKey, clientId, controller, completion in
+      service.apiKey = apiKey
+      GIDSignIn.sharedInstance.signIn(
+        with: .init(clientID: clientId),
+        presenting: controller,
+        callback: { user, error in
+          if let error {
+            completion(.failure(DriveClientError.signIn(error)))
+            return
+          }
+          guard user != nil else {
+            completion(.failure(DriveClientError.unknown))
+            return
+          }
           completion(.success(()))
         }
-      },
-      _signIn: { apiKey, clientId, controller, completion in
-        service.apiKey = apiKey
-        GIDSignIn.sharedInstance.signIn(
-          with: .init(clientID: clientId),
-          presenting: controller,
-          callback: { user, error in
-            if let error {
-              completion(.failure(DriveClientError.signIn(error)))
-              return
-            }
-            guard user != nil else {
-              completion(.failure(DriveClientError.unknown))
-              return
-            }
-            completion(.success(()))
-          }
-        )
-      }
-    )
-  }
+      )
+    }
+  )
 }
 
 extension CloudFilesManager {
   public static func drive(
-    apikey: String,
+    apiKey: String,
     clientId: String
   ) -> CloudFilesManager {
     CloudFilesManager(
       link: .drive(
-        apiKey: apikey,
+        apiKey: apiKey,
         clientId: clientId
       ),
-      fetch: .drive(),
-      upload: .unimplemented,
+      fetch: .drive(fileName: "backup.xxm"),
+      upload: .drive(path: "/backup"),
       unlink: .drive(),
       enable: .unimplemented,
       disable: .unimplemented,
-      download: .unimplemented,
+      download: .drive(),
       isLinked: .drive(),
       isEnabled: .unimplemented
     )
--- Sources/CloudFilesDrive/DriveOperations.swift
+++ Sources/CloudFilesDrive/DriveOperations.swift
@@ -3,52 +3,60 @@ import CloudFiles
 
 extension Unlink {
   public static func drive(
-    client: DriveClient = .live()
+    client: DriveClient = .live
   ) -> Unlink {
     Unlink { client.unlink() }
   }
 }
 
+extension Download {
+  public static func drive(
+    client: DriveClient = .live
+  ) -> Download {
+    Download { _ in
+      // TODO
+    }
+  }
+}
+
+extension Upload {
+  public static func drive(
+    path: String,
+    client: DriveClient = .live
+  ) -> Upload {
+    Upload { data, completion in
+      client.upload(
+        path: path,
+        input: data,
+        completion: completion
+      )
+    }
+  }
+}
+
 extension Fetch {
   public static func drive(
-    client: DriveClient = .live()
+    client: DriveClient = .live,
+    fileName: String
   ) -> Fetch {
     Fetch { completion in
-      client.listFolder(name: "backup") { getFolderResult in
-        switch getFolderResult {
-        case .success(let folder):
-          client.listFiles(at: folder) { listFilesResult in
-            switch listFilesResult {
-            case .success:
-              client.fetch(fileName: "backup.xxm") { fetchResult in
-                switch fetchResult {
-                case .success:
-                  break
-                case .failure(let error):
-                  completion(.failure(error))
-                }
-              }
-            case .failure(let error):
-              completion(.failure(error))
-            }
-          }
-        case .failure(let error):
-          completion(.failure(error))
-        }
-      }
+      client.fetch(
+        fileName: fileName,
+        completion: completion
+      )
     }
   }
 }
 
 extension IsLinked {
-  public static func drive(client: DriveClient = .live()) -> IsLinked {
+  public static func drive(client: DriveClient = .live) -> IsLinked {
     IsLinked { client.isLinked() }
   }
 }
 
 extension Link {
   public static func drive(
-    client: DriveClient = .live(),
+    client: DriveClient = .live,
     apiKey: String,
     clientId: String
   ) -> Link {
