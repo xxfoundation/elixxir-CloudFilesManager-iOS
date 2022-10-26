a0d31abf2adabd6049105d664553d37fa2cc0b53
Bruno Muniz Azevedo Filho <bruno@elixxir.io>
2022-10-26 17:30:34 -0300

    not storing linked services on settings

--- Sources/CloudFiles/CloudSettings.swift
+++ Sources/CloudFiles/CloudSettings.swift
@@ -5,19 +5,16 @@ public struct CloudSettings: Equatable, Codable {
   public var automaticBackups: Bool
   public var enabledService: CloudService?
   public var backups: [CloudService: Fetch.Metadata]
-  public var connectedServices: [CloudService: CloudFilesManager]
 
   public init(
     wifiOnlyBackup: Bool = false,
     automaticBackups: Bool = false,
     enabledService: CloudService? = nil,
-    backups: [CloudService: Fetch.Metadata] = [:],
-    connectedServices: [CloudService: CloudFilesManager] = [:]
+    backups: [CloudService: Fetch.Metadata] = [:]
   ) {
     self.wifiOnlyBackup = wifiOnlyBackup
     self.automaticBackups = automaticBackups
     self.enabledService = enabledService
-    self.connectedServices = connectedServices
     self.backups = backups
   }
 
@@ -31,16 +28,14 @@ public struct CloudSettings: Equatable, Codable {
         wifiOnlyBackup: settings.wifiOnlyBackup,
         automaticBackups: settings.automaticBackups,
         enabledService: settings.enabledService,
-        backups: settings.backups,
-        connectedServices: settings.connectedServices
+        backups: settings.backups
       )
     } else {
       self.init(
         wifiOnlyBackup: false,
         automaticBackups: true,
         enabledService: nil,
-        backups: [:],
-        connectedServices: [:]
+        backups: [:]
       )
     }
   }
